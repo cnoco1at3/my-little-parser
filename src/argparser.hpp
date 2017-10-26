6b7c91c2da7375d898a854eefa013372636c7382
Yiheng Zhu <yiheng.zhu@midas.team>
2017-10-26 09:55:05 -0700

    add supported types

--- src/argparser.hpp
+++ src/argparser.hpp
@@ -3,32 +3,40 @@
 
 
 #include <fstream>
-#include <unordered_map>
-#include <string>
 
 
-class ArgumentParser
+namespace mylittleparser
 {
-public:
-    constexpr ArgumentParser (int argc, char** argv, char** envp)
-        : m_argc (argc), m_argv (argv), m_envp (envp)
-    { }
-
-    constexpr void addArgument (const char* name, const char* help = "")
+    class ArgumentParser
     {
-        return;
-    }
+    public:
+        constexpr ArgumentParser (int argc, char** argv, char** envp = nullptr)
+            : m_argc (argc), m_argv (argv), m_envp (envp)
+        { }
 
-    std::unordered_map<std::string, std::string> parseArgs ()
-    {
-        return std::unordered_map<std::string, std::string>();
-    }
+        template<typename T>
+        struct SupportedType { static const bool value = false; };
 
-private:
-    int m_argc;
-    const char** m_argv;
-    const char** m_envp;
-};
+        template<> struct SupportedType<bool> { static const bool value = true; };
+        template<> struct SupportedType<int> { static const bool value = true; };
+        template<> struct SupportedType<char*> { static const bool value = true; };
+        template<> struct SupportedType<const char*> { static const bool value = true; };
+
+        template <typename T, int nargs = 0, bool required = false>
+        typename std::enable_if<
+            SupportedType<typename std::remove_const<T>::type>::value
+        >::type
+            add_argument (const char* name, const char* help, const char* abbr = nullptr)
+        { 
+            printf ("%s %s", name, help);
+        }
+
+    private:
+        int m_argc;
+        char** m_argv;
+        char** m_envp;
+    };
+}
 
 
 #endif
