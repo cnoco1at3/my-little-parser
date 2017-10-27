f3233f23524def969ce48f9e8feb4c89b1b9ffbb
Yiheng Zhu <yiheng.zhu@midas.team>
2017-10-27 00:06:51 -0700

    Basic working arg parser

--- src/argparser.hpp
+++ src/argparser.hpp
@@ -3,37 +3,125 @@
 
 
 #include <fstream>
+#include <iostream>
 #include <unordered_map>
 
 
 namespace mylittleparser
 {
+    class ParseEntry
+    {
+    public:
+
+
+        ParseEntry () : data_ (nullptr), size_ (0), nargs_ (0) { }
+
+
+        ~ParseEntry () { if (data_) delete[] data_; data_ = nullptr; }
+
+
+        template<typename T>
+        inline T operator[](int idx) { return get<T> (idx); }
+
+
+        template<typename T>
+        inline T get (int idx)
+        {
+            if (sizeof (T) != size_ || 0 > idx || nargs_ <= idx) throw this;
+            return reinterpret_cast<T*>(data_)[idx];
+        }
+
+
+    private:
+
+
+        friend class ArgumentParser;
+
+
+        template<typename T>
+        inline void resize (size_t nargs)
+        {
+            size_ = sizeof (T);
+            nargs_ = nargs;
+            if (data_) delete[] data_;
+            data_ = new unsigned char[size_ * nargs_];
+        }
+
+
+        template<typename T>
+        inline void set (int idx, const T& val)
+        {
+            reinterpret_cast<T*>(data_)[idx] = val;
+        }
+
+
+        unsigned char* data_;
+        size_t size_;
+        size_t nargs_;
+    };
+
+
+    typedef std::unordered_map<std::string, ParseEntry> ParseArgs;
+
+
     class ArgumentParser
     {
     public:
-        constexpr ArgumentParser (int argc, char** argv)
+
+
+        ArgumentParser (int argc, char** argv)
             : argc_ (argc), argv_ (argv)
         { }
+
+
+        ~ArgumentParser ()
+        {
+
+        }
+
 
         template <typename T, int nargs = 0, bool required = false>
         void add_argument (const char* name, const char* help,
                            const char* abbr = nullptr,
-                           typename std::decay<decltype(ParseType<T, nargs>::parse)>::type* = nullptr)
+                           typename std::decay<decltype(
+                               ParseType<std::remove_cv<T>::type,
+                               nargs>::parse)>::type* = nullptr)
         {
-            ParseType<T, nargs>::parse ("");
+            typedef std::remove_cv<T>::type U;
+
+            int idx;
+            for (idx = 1; idx < argc_; ++idx)
+                if (strcoll (name, argv_[idx]) == 0) break;
+
+            if (required && argc_ == idx) { throw name; }
+
+            parse_args_[name].resize<U> (nargs);
+            for (int i = 1; i <= nargs && idx + i < argc_; ++i)
+                parse_args_[name].set<U> (
+                    i - 1, ParseType<U, nargs>::parse (argv_[idx + i]));
         }
 
+
+        ParseArgs parse_args () { return std::move (parse_args_); }
+
+
     private:
+
+
         template<typename T, int nargs> struct ParseType { };
+
 
         template<int nargs> struct ParseType<bool, nargs>
         {
             static typename std::enable_if<nargs == 0, bool>::type
                 parse (const char* str)
             {
-                return str != nullptr;
+                return true;
             }
         };
+
+
+
         template<int nargs> struct ParseType<int, nargs>
         {
             static typename std::enable_if<nargs >= 1, int>::type
@@ -42,6 +130,8 @@ namespace mylittleparser
                 return std::atoi (str);
             }
         };
+
+
         template<int nargs> struct ParseType<char*, nargs>
         {
             static typename std::enable_if<nargs >= 1, char*>::type
@@ -50,6 +140,8 @@ namespace mylittleparser
                 return const_cast<char*>(str);
             }
         };
+
+
         template<int nargs> struct ParseType<const char*, nargs>
         {
             static typename std::enable_if<nargs >= 1, const char*>::type
@@ -62,6 +154,7 @@ namespace mylittleparser
 
         int argc_;
         char** argv_;
+        std::unordered_map<std::string, ParseEntry> parse_args_;
     };
 }
 
--- test/main.cpp
+++ test/main.cpp
@@ -4,8 +4,10 @@
 int main (int argc, char** argv)
 {
     mylittleparser::ArgumentParser parser (argc, argv);
-    parser.add_argument<bool, 0> (nullptr, nullptr, "");
-    parser.add_argument<int, 0> (nullptr, nullptr);
+    parser.add_argument<const int, 2> ("--aws", nullptr);
+    parser.add_argument<const char*, 2> ("--sth", nullptr);
+    mylittleparser::ParseArgs parse_args = parser.parse_args ();
+    std::cout << parse_args["--sth"].get<const char*> (1);
     getchar ();
     return 0;
 }
