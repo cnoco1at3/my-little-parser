eaa2bd9b8e299d80c853e374e896a49dce1a47bb
Yiheng <yiheng@midas.team>
2017-10-27 17:43:18 -0700

    Gave ability to support post processing

--- src/argparser.hpp
+++ src/argparser.hpp
@@ -15,16 +15,16 @@ namespace mylittleparser
         ParseEntry () : data_ (nullptr), size_ (0), nargs_ (0) { }
 
 
-        ~ParseEntry () { if (data_) delete[] data_; data_ = nullptr; }
-
-
-        template<typename T>
-        inline T operator[](int idx) { return get<T> (idx); }
+        ~ParseEntry () { if (data_) delete[] data_; }
 
 
         template<typename T>
         inline T get (int idx)
         {
+            /*!
+                \note This only provide basic type safety, we still don't have
+                      the capability to know the type of the stored data.
+            */
             if (sizeof (T) != size_ || 0 > idx || (int) nargs_ <= idx)
                 throw this;
             return reinterpret_cast<T*>(data_)[idx];
@@ -37,18 +37,16 @@ namespace mylittleparser
         friend class ArgumentParser;
 
 
-        template<typename T>
-        inline void resize (size_t nargs)
+        template<typename T, int nargs> inline void resize ()
         {
             size_ = sizeof (T);
             nargs_ = nargs;
             if (data_) delete[] data_;
-            data_ = new unsigned char[size_ * nargs_];
+            data_ = new unsigned char[size_ * nargs_] ();
         }
 
 
-        template<typename T>
-        inline void set (int idx, const T& val)
+        template<typename T> inline void set (int idx, const T& val)
         {
             reinterpret_cast<T*>(data_)[idx] = val;
         }
@@ -65,123 +63,165 @@ namespace mylittleparser
 
     class ArgumentParser
     {
-    public:
-
-
-        ArgumentParser (int argc, char** argv)
-            : argc_ (argc), argv_ (argv), help_(false)
-        { 
-            help_ = search<false> ("--help", "-h") < argc_;
-        }
-
-
-        ~ArgumentParser ()
-        {
-
-        }
-
-
-        template <typename T, int nargs = 0, bool required = false>
-        void add_argument (const char* name, const char* help,
-                           const char* abbr = nullptr,
-                           typename std::decay<decltype(
-                               ParseType<std::remove_cv<T>::type,
-                               nargs>::parse)>::type* = nullptr)
-        {
-            typedef std::remove_cv<T>::type U;
-
-            int idx = search<required> (name, abbr);
-            parse<U, nargs> (name, idx);
-        }
-
-
-        ParseArgs parse_args () 
-        { 
-            if (help_) exit (0);
-            return parse_args_; 
-        }
-
-
     private:
 
 
-        template<bool required> int search (const char* name, const char* abbr)
+        template<int type> struct ParseType;
+
+
+        // base type
+        template<typename T, int nargs = 0, class enable = void> struct TypeId
         {
-            int idx = 1;
-            while (idx < argc_ && strcoll (name, argv_[idx]) != 0) idx++;
-            return idx;
-        }
-
-
-        template<> int search<true> (const char* name, const char* abbr)
-        {
-            int idx = search<false> (name, abbr);
-            if (argc_ == idx) { throw this; }
-            return idx;
-        }
-
-
-        template<typename U, int nargs> void parse (const char* name, int idx)
-        {
-            parse_args_[name].resize<U> (nargs);
-            for (int i = 1; i <= nargs && idx + i < argc_; ++i)
-                parse_args_[name].set<U> (
-                    i - 1, ParseType<U, nargs>::parse (argv_[idx + i]));
-        }
-
-
-        template<> void parse<bool, 0> (const char* name, int idx)
-        {
-            parse_args_[name].resize<bool> (1);
-            parse_args_[name].set<bool> (0, idx < argc_);
-        }
-
-
-        template<typename T, int nargs> struct ParseType { };
-
-
-        template<int nargs> struct ParseType<bool, nargs>
-        {
-            static typename std::enable_if<nargs == 0, bool>::type parse () { }
+            static const int value = -1;
         };
 
 
-
-        template<int nargs> struct ParseType<int, nargs>
+        // bool type
+        template<int nargs>
+        struct TypeId <bool, nargs, typename std::enable_if<nargs == 0>::type>
         {
-            static typename std::enable_if<nargs >= 1, int>::type
-                parse (const char* str)
-            {
-                return std::atoi (str);
-            }
+            static const int value = 0;
         };
 
 
-        template<int nargs> struct ParseType<char*, nargs>
+        // int type
+        template<int nargs>
+        struct TypeId <int, nargs, typename std::enable_if<nargs >= 1>::type>
         {
-            static typename std::enable_if<nargs >= 1, char*>::type
-                parse (const char* str)
-            {
-                return const_cast<char*>(str);
-            }
+            static const int value = 1;
         };
 
 
-        template<int nargs> struct ParseType<const char*, nargs>
+        // c style string type
+        template<int nargs>
+        struct TypeId <char *, nargs, typename std::enable_if<nargs >= 1>::type>
         {
-            static typename std::enable_if<nargs >= 1, const char*>::type
-                parse (const char* str)
-            {
-                return str;
-            }
+            static const int value = 2;
         };
 
 
-        int argc_;
-        char** argv_;
+        // c style const string type
+        template<int nargs>
+        struct TypeId <const char *, nargs,
+            typename std::enable_if<nargs >= 1>::type>
+        {
+            static const int value = 3;
+        };
+
+
+        template<bool required> int search (const char* name, const char* abbr);
+
+
+        template<typename U, int nargs> void parse (const char* name, int idx);
+
+
+        int argc_;                                  //!< arguments count
+        char** argv_;                               //!< arguments values
+        std::unordered_map<std::string, int> argi_; //!< arguments indices
         bool help_;
         ParseArgs parse_args_;
+
+
+    public:
+
+
+        ArgumentParser (int argc = __argc, char** argv = __argv)
+            : argc_ (argc), argv_ (argv), help_ (false)
+        {
+            for (int i = 0; i < argc; ++i) argi_[argv[i]] = i;
+            help_ = search<false> ("--help", "-h") != argc_;
+        }
+
+
+        ~ArgumentParser () { }
+
+
+        template <typename T, int nargs = 0, bool required = false>
+        typename std::enable_if <
+            TypeId<typename std::remove_cv<T>::type, nargs>::value >= 0>::type
+            add_argument (const char* name,
+                          const char* help,
+                          const char* abbr = nullptr)
+        {
+            typedef typename std::remove_cv<T>::type U;
+
+            parse<U, nargs> (name, search<required> (name, abbr));
+        }
+
+
+        ParseArgs parse_args ()
+        {
+            if (help_) exit (0);
+            return parse_args_;
+        }
+
     };
+
+
+    template<> struct ArgumentParser::ParseType<
+        ArgumentParser::TypeId<bool, 0>::value>
+    {
+        static bool parse (const char* str) { return true; }
+    };
+
+
+    template<>
+    struct ArgumentParser::ParseType<ArgumentParser::TypeId<int, 1>::value>
+    {
+        static int parse (const char* str) { return std::atoi (str); }
+    };
+
+
+    template<> struct ArgumentParser::ParseType<
+        ArgumentParser::TypeId<char*, 1>::value>
+    {
+        static char *parse (const char *str)
+        {
+            return const_cast<char*>(str);
+        }
+    };
+
+
+    template<> struct ArgumentParser::ParseType<
+        ArgumentParser::TypeId<const char*, 1>::value>
+    {
+        static const char *parse (const char *str) { return str; }
+    };
+
+
+    template<bool required>
+    int ArgumentParser::search (const char* name, const char* abbr)
+    {
+        return argi_.find (name) != argi_.end () ? argi_.at (name) : argc_;
+    }
+
+
+    template<>
+    int ArgumentParser::search<true> (const char* name, const char* abbr)
+    {
+        if (argi_.find (name) == argi_.end ()) throw this;
+        return argi_.at (name);
+    }
+
+
+    template<typename U, int nargs>
+    inline void ArgumentParser::parse (const char* name, int idx)
+    {
+        parse_args_[name].resize<U, nargs> ();
+        for (int i = 1; i <= nargs && idx + i < argc_; ++i)
+            parse_args_[name].set<U> (
+                i - 1,
+                ParseType <TypeId<U, nargs>::value>::parse (
+                    argv_[idx + i]));
+    }
+
+
+    template<>
+    inline void ArgumentParser::parse<bool, 0> (const char* name, int idx)
+    {
+        parse_args_[name].resize<bool, 1> ();
+        parse_args_[name].set<bool> (0, idx < argc_);
+    }
 }
 
 
--- test/main.cpp
+++ test/main.cpp
@@ -4,12 +4,14 @@
 
 int main (int argc, char** argv)
 {
-    mylittleparser::ArgumentParser parser (argc, argv);
-    parser.add_argument<const int, 2> ("--aws", nullptr);
+    mylittleparser::ArgumentParser parser;
+    parser.add_argument<int, 2> ("--aws", nullptr);
     parser.add_argument<const char*, 2> ("--sth", nullptr);
     parser.add_argument<bool> ("--world", nullptr);
     mylittleparser::ParseArgs parse_args = parser.parse_args ();
     std::cout << (parse_args["--world"].get<bool> (0) ? "true" : "false");
+    std::cout << parse_args["--aws"].get<int> (1);
     getchar ();
+
     return 0;
 }
